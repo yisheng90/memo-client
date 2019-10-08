9daced826c161e20a01db675f39f09dbf22bf7ee
Yi Sheng <yisheng@yishenglee.com>
2019-10-08 22:04:55 +0800

    Updates

---
+++ precache-manifest.f0fc6b3ddbfc45a9f8d2fd82d9db96c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8a3a4a04d0f3815ee26119236d2d51",
+    "url": "/memo-client/index.html"
+  },
+  {
+    "revision": "210aedb8d7b032fa6ef1",
+    "url": "/memo-client/static/css/main.a0e5010a.chunk.css"
+  },
+  {
+    "revision": "5a7be606c10b79092054",
+    "url": "/memo-client/static/js/2.66454f2c.chunk.js"
+  },
+  {
+    "revision": "210aedb8d7b032fa6ef1",
+    "url": "/memo-client/static/js/main.c626d7b0.chunk.js"
+  },
+  {
+    "revision": "af2b735479ababbafe95",
+    "url": "/memo-client/static/js/runtime-main.a73ef31b.js"
+  }
+]);
