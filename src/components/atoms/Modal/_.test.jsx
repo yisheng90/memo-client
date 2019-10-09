d49454eeb7b9b139a5f3f28a94e7e7a95ade2651
Yi Sheng Lee <yisheng@yishenglee.com>
2019-10-09 14:01:36 +0800

    add test to modal

---
+++ src/components/atoms/Modal/_.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { render, fireEvent, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { lorem } from 'faker'
+import { NotificationCard } from './index'
+
+const mockOnCancel = jest.fn()
+
+describe('NotificationCard', () => {
+  afterEach(() => cleanup())
+
+  it('should render NotificationCard', () => {
+    const expectedText = lorem.sentence()
+    const { getByTestId, getByText } = render(
+      <NotificationCard message={expectedText} onCancel={mockOnCancel} />
+    )
+
+    expect(getByTestId('notification__card')).toBeTruthy()
+    expect(getByText(expectedText)).toBeTruthy()
+  })
+
+  it('should trigger onCancel when the cancel button is clicked', () => {
+    const { getByTestId } = render(
+      <NotificationCard message={lorem.sentence()} onCancel={mockOnCancel} />
+    )
+    const closeButton = getByTestId('notification__card--close')
+
+    fireEvent.click(closeButton)
+
+    expect(mockOnCancel).toHaveBeenCalled()
+  })
+})
